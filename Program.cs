ca6f1aa287aab5d78e3d1c108cd6d5151bd2e614
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-08-15 18:44:43 -0300

    Criei o primeiro aplicativo de console .NET

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicios_URI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
