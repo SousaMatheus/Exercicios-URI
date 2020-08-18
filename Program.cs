6bce89630248a903e65a133d0cc29914d1cc775e
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-08-17 22:42:15 -0300

    Declarado variáveis e adicionado arquivos do projeto

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace Exercicios_URI
+namespace Exercicios_URI 
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        int a, b, m;
+            string []vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]); 
+
+            switch (m)
+            { case 1: 
+            
+
+                
+                default:
+            }
+            
         }
     }
 }
