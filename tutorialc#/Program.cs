ab2bf70113526d1b31cf679a0d7bf82ffbf5fd8d
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-08-20 10:41:08 -0300

    Novo programa adicionado sobre tutoriais

---
+++ tutorialc#/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tutorialc_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+
+            console.writeline($"My friends are {firstfriend} and {secondFriend}");
+        }
+    }
+}
