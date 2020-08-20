f61da5820c7e6de0ae120b6020099726dd28951c
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-08-20 11:25:24 -0300

    corrigido comandos em minusculo

--- tutorialc#/Program.cs
+++ tutorialc#/Program.cs
@@ -4,12 +4,13 @@ namespace tutorialc_
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             string firstFriend = "Maria";
             string secondFriend = "Sage";
 
-            console.writeline($"My friends are {firstfriend} and {secondFriend}");
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
         }
     }
 }
