ecc784d16a4b07b118702b302e8023fbb342d65b
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-08-20 21:17:55 -0300

    Agora está certo pow

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace URI 1044
 {
     class Program
     {
@@ -9,30 +9,13 @@ namespace HelloWorld
             int a, b;
             string [] vet = Console.ReadLine().Split(' ');
             a = int.Parse(vet[0]);
-<<<<<<< HEAD
             b = int.Parse(vet[1]);
             if (a % b == 0 || b % a == 0){
                 Console.WriteLine("Sao Multiplos");
             } else {
                 Console.WriteLine("Nao sao Multiplos");
             }
-            Console.ReadLine();
-=======
-            b = int.Parse(vet[1]); 
-
-            if ( a % b == 0 || b % a == 0){
-                Console.Writeline("Sao Multiplos");
-                
-            }
-            else {
-                Console.Writeline("Nao sao Multiplos")
-            }
-        }
-        console.ReadLine();
-<<<<<<< HEAD
->>>>>>> c9a50b7bfef8310ebf2b41b14c66edc92b442b47
-=======
->>>>>>> refs/remotes/origin/master
+            
     }
 }
 }
